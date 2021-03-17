701293d3df6249e2119ccbb6452d00cf401a6770
Thibault DUPONCHELLE <thibault.duponchelle@amadeus.com>
2021-03-17 18:59:06 +0100

    This is automatix :robot:

--- lib/Acme/Automatix.pm
+++ lib/Acme/Automatix.pm
@@ -6,90 +6,26 @@ use warnings;
 
 =head1 NAME
 
-Acme::Automatix - The great new Acme::Automatix!
+Acme::Automatix - The release of Acme::Automatix is automatix!
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Quick summary of what the module does...
 
-Perhaps a little code snippet.
-
-    use Acme::Automatix;
-
-    my $foo = Acme::Automatix->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+Nothing!
 
 =head1 AUTHOR
 
 Thibault Duponchelle, C<< <thibault.duponchelle at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-acme-automatix at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-Automatix>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Acme::Automatix
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-Automatix>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/Acme-Automatix>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/Acme-Automatix>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 
 =head1 LICENSE AND COPYRIGHT
 
